62bb4193415a52f1ce51a59dafc6335121e5cf0c
anydream <anydreamplay@hotmail.com>
2017-10-11 08:57:36 +0800

    Initial code commit

---
+++ include/ArmInt.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <stdint.h>
+
+//////////////////////////////////////////////////////////////////////////
+// ARM 解释器
+class ArmInt
+{
+public:
+	ArmInt();
+	~ArmInt();
+
+	void Step();
+
+private:
+	void DispatchInst();
+
+	struct OpCode
+	{
+		uint16_t Code;
+		uint16_t Op1;
+		uint16_t Op2;
+		uint16_t Op3;
+	};
+
+	OpCode DecodeInst();
+	OpCode DecodeThumb();
+};
---
+++ src/BinReader.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#include <stdint.h>
+
+//////////////////////////////////////////////////////////////////////////
+// 二进制数据读取器
+class BinReader
+{
+public:
+	explicit BinReader(const void *ptr, size_t len)
+		: Buffer_(static_cast<const uint8_t*>(ptr))
+		, Length_(len)
+	{}
+
+	template <class T>
+	bool Read(T &result)
+	{
+		const size_t sz = sizeof(T);
+		if (Offset_ + sz <= Length_)
+		{
+			result = *static_cast<const T*>(Buffer_ + Offset_);
+			Offset_ += sz;
+			return true;
+		}
+		return false;
+	}
+
+	template <class T>
+	const T& Read()
+	{
+		const size_t sz = sizeof(T);
+		if (Offset_ + sz <= Length_)
+		{
+			const size_t oldPos = Offset_;
+			Offset_ += sz;
+			return *static_cast<const T*>(Buffer_ + oldPos);
+		}
+		return T();
+	}
+
+	void Advance(size_t sz)
+	{
+		if (Offset_ + sz <= Length_)
+			Offset_ += sz;
+	}
+
+	bool Seek(size_t pos)
+	{
+		if (pos <= Length_)
+		{
+			Offset_ = pos;
+			return true;
+		}
+		return false;
+	}
+
+	size_t Tell() const
+	{
+		return Offset_;
+	}
+
+	size_t Remain() const
+	{
+		return Length_ - Offset_;
+	}
+
+	size_t Size() const
+	{
+		return Length_;
+	}
+
+private:
+	const uint8_t *Buffer_ = nullptr;
+	size_t Length_ = 0;
+	size_t Offset_ = 0;
+};
